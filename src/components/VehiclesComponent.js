c4420c64bffd7aa2b90b4f90c93609d354ece7f1
Dimitar <53209562+mitacheto@users.noreply.github.com>
2022-10-18 13:02:02 +0300

    removed unused variable

--- src/components/VehiclesComponent.js
+++ src/components/VehiclesComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function VehiclesComponent({ fetchedData, imagesData }) {
+export default function VehiclesComponent({ fetchedData }) {
     return (
         <div className='results'>
             {fetchedData &&
